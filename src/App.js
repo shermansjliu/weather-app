d33769f2c9ba7ae86dc24dc52e215c1eeddf3bad
Sherman Liu <shermanliu03@gmail.com>
2020-09-03 12:45:55 -0400

    implemented base code for taliwind with styled components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+export default function App() {
+  const [temperature, setTemperature] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(
+          "https://api.openweathermap.org/data/2.5/weather?q=London&appid=607e75a15955b047dee2aa02c66f43a6",
+          { mode: "cors" }
+        );
+        const json = await response.json();
+        console.log(json);
+      } catch (error) {}
+    }
+    fetchData();
+  }, []);
+console.log(temperature);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav></nav>
+      
+    </>
   );
 }
-
-export default App;
